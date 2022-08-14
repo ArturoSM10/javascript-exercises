6ff1469cb4986612768cf82cf97f8fa8f269abd5
Arturo <jarsmdza@gmail.com>
2022-08-14 14:25:49 -0500

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let rounded = ((temp-32)/1.8);
+  return Math.round(rounded * 10) / 10;;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let rounded = ((temp*1.8)+32)
+  return Math.round(rounded * 10) / 10;;
 };
 
 // Do not edit below this line
