95296d427d696c70d322470613a67d0438624acd
Arturo <jarsmdza@gmail.com>
2022-08-13 20:09:35 -0500

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(yourText = "No valid option", numberOfTimes = 0) {
+    if (numberOfTimes<0) return 'ERROR';
+    let string = '';
+    for (let i = 0; i<numberOfTimes; i++) {
+        string += yourText;
+    }
+    return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
